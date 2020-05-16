7ad8e0c41302d9aaec1bfa83c8da09ffa2b8b00b
Josh Newsom <newsommeister@gmail.com>
2020-05-15 23:52:20 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'skrabble';
+}
